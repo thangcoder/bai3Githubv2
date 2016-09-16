541f0fbb25514b9cb806fc42f95ec46ebee8aa9a
Thangcoder <trangchongchengit@gmail.com>
2016-09-16 23:10:45 +0700

    commit changed

---
+++ app/src/main/java/thangcoder/com/bai3githubv2/LoginActivity.java
@@ -0,0 +1,9 @@
+package thangcoder.com.bai3githubv2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 16/09/2016.
+ */
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/thangcoder/com/bai3githubv2/Student.java
@@ -0,0 +1,7 @@
+package thangcoder.com.bai3githubv2;
+
+/**
+ * Created by Administrator on 16/09/2016.
+ */
+public class Student {
+}
